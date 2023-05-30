5d14ec776fcab27733e76a3a9073dd2828d89a06
HGSC <HGSC@LP1619MINGG.ad.hgsc.bcm.edu>
2023-05-30 16:55:33 -0500

    separe the functions for ulti and single vcf files

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-"# vcf_visualizer" 
+# VCF Chart Data Generator
+
+The VCF Chart Data Generator is a Python class that provides functionality for generating allele frequency charts and sample SV numbers from input VCF files. 
+
+## Requirements
+
+The following dependencies are required to use the Genome Chart Data Generator:
+
+- Python 3.x
+- matplotlib
+- upsetplot
+
+## Installation
+To install the VCF Chart Data Generator and its dependencies, you can use pip.
--- multi_vcf_visualizer.py
+++ multi_vcf_visualizer.py
@@ -6,18 +6,18 @@ Created on Mon May 15 10:17:54 2023
 """
 import subprocess
 from arg_parser import argument_parser
-from functions_variables import *
+from functions_variables_multi import *
 
 
 def main():
     input_vcf_file,output_chrt=argument_parser()
-    vcf_variables=vcf_number_variants(input_vcf_file)
-    allele_frequency_chart_genrator(input_vcf_file,output_chrt)
-    samples_sv_numbers(input_vcf_file,output_chrt)
+#    allele_frequency_chart_genrator(input_vcf_file,output_chrt)
+#    samples_sv_numbers(input_vcf_file,output_chrt)
+    Genome_chart_data_generator=GenomeChartDataGenerator(input_vcf_file,output_chrt)
+#    Genome_chart_data_generator.allele_frequency_chart_generator()
+    Genome_chart_data_generator.samples_sv_numbers()
     cmd= f"rm -f  {output_chrt}*.txt"
     subprocess.run(cmd, shell=True)
-# Entry point of the program
-
 if __name__ == "__main__":
     main()
 
