b15b6f24480bd341fa01f0f87cb153965a581160
HGSC <HGSC@LP1619MINGG.ad.hgsc.bcm.edu>
2023-06-12 10:19:59 -0500

    update the codes

--- functions_variables_single.py
+++ functions_variables_single.py
@@ -15,12 +15,36 @@ from upsetplot import from_memberships
 from DataClasses import *
 from dataclasses import dataclass
 
-ranges = [(50, 100), (100,200),(200,300),(300,400),(400,600),(600,800),(800,1000),(1000,2500),(2500,5000),(5000,10000000)]
-x_labels = ['50-100', '100-200', '200-300','300-400','400-600','600-800','800-1k','1k-2.5k','2.5k-5k','>5k']
+ranges = [
+    (50, 100),
+    (100, 200),
+    (200, 300),
+    (300, 400),
+    (400, 600),
+    (600, 800),
+    (800, 1000),
+    (1000, 2500),
+    (2500, 5000),
+    (5000, 10000000),
+]
+x_labels = [
+    "50-100",
+    "100-200",
+    "200-300",
+    "300-400",
+    "400-600",
+    "600-800",
+    "800-1k",
+    "1k-2.5k",
+    "2.5k-5k",
+    ">5k",
+]
 
-def separate_lists(lst,num):
+
+def separate_lists(lst, num):
     elements = [item[num] for item in lst]
     return elements
+
 
 def count_numbers_in_ranges(numbers, ranges):
     counts = [0] * len(ranges)
@@ -31,7 +55,8 @@ def count_numbers_in_ranges(numbers, ranges):
                 break
     return counts
 
-def genome_bar_chart(output_file_path,labels,*bars:GenomeChartData):
+
+def genome_bar_chart(output_file_path, labels, *bars: GenomeChartData):
     x = np.arange(len(labels))
     # Define the width of each bar
     width = 0.2
@@ -39,96 +64,107 @@ def genome_bar_chart(output_file_path,labels,*bars:GenomeChartData):
     plt.bar(x - width, bars[0].count(labels), width=width, label=bars[0].legend)
     plt.bar(x, bars[1].count(labels), width=width, label=bars[1].legend)
     # Add labels and title
-    plt.xlabel("Genotype",size=6)
-    plt.ylabel("Count",size=6)
+    plt.xlabel("Genotype", size=6)
+    plt.ylabel("Count", size=6)
     plt.title("Genotype_Frequency")
     # Set the x-axis tick labels
     plt.xticks(x, labels)
     # Add a legend
     plt.legend()
-    plt.savefig(output_file_path,dpi=800)
+    plt.savefig(output_file_path, dpi=800)
 
-def sv_size_type_chart(l1,l2,label_1,label_2,path_2_chart):
+
+def sv_size_type_chart(l1, l2, label_1, label_2, path_2_chart):
     fig, ax = plt.subplots()
     x = np.arange(len(l1))
     bar_width = 0.40
     plt.bar(x, l1, width=bar_width, label=label_1)
     plt.bar(x + bar_width, l2, width=bar_width, label=label_2)
-    plt.xlabel('SizeBin')
-    plt.ylabel('Count')
-    plt.title('SV Size/Type Distribution')
-    plt.xticks(x + bar_width/5, x)
-    ax.set_xticklabels(x_labels,rotation=45)
+    plt.xlabel("SizeBin")
+    plt.ylabel("Count")
+    plt.title("SV Size/Type Distribution")
+    plt.xticks(x + bar_width / 5, x)
+    ax.set_xticklabels(x_labels, rotation=45)
     plt.tight_layout()  # Adjust the padding to accommodate all labels
-    plt.legend(title='svtype')
-    plt.savefig(path_2_chart,dpi=800)
+    plt.legend(title="svtype")
+    plt.savefig(path_2_chart, dpi=800)
     plt.figure().clear()
     plt.close()
     plt.cla()
     plt.clf()
 
-def lenght_var_count_chart(output_name,del_flag,v_s_list,min_val,max_value,chart_pos,ytick_flag,xtick_locs_list,plt_titl,subplt_title):
 
-    plt.subplot(1,6, chart_pos)
-    plt.subplots_adjust(wspace=0.05, hspace=0.05,bottom=0.3)
+def lenght_var_count_chart(
+    output_name,
+    del_flag,
+    v_s_list,
+    min_val,
+    max_value,
+    chart_pos,
+    ytick_flag,
+    xtick_locs_list,
+    plt_titl,
+    subplt_title,
+):
+    plt.subplot(1, 6, chart_pos)
+    plt.subplots_adjust(wspace=0.05, hspace=0.05, bottom=0.3)
 
     minimum = min_val
     maximum = max_value
     selected_values = [x for x in v_s_list if minimum <= x <= maximum]
     # Determine the bin size
-    bin_size = round((max(selected_values) - min(selected_values))/100)
+    bin_size = round((max(selected_values) - min(selected_values)) / 100)
     # Create bins based on the bin size
     bins = np.arange(min(selected_values), max(selected_values) + bin_size, bin_size)
     # Count the number of values that fall into each bin
     bin_counts, _ = np.histogram(selected_values, bins)
     # Display the bin plot
-    plt.bar(bins[:-1], bin_counts, width=bin_size, align='edge')
+    plt.bar(bins[:-1], bin_counts, width=bin_size, align="edge")
     xtick_labels = [str(x) for x in xtick_locs_list]
-    plt.xticks(xtick_locs_list, xtick_labels,rotation=90)
-    if chart_pos==1:
-        plt.ylabel('Counts')
-    if chart_pos==3:
-        plt.xlabel('\n\n        Lenght of Variants')
-    plt.yscale('log')
+    plt.xticks(xtick_locs_list, xtick_labels, rotation=90)
+    if chart_pos == 1:
+        plt.ylabel("Counts")
+    if chart_pos == 3:
+        plt.xlabel("\n\n        Lenght of Variants")
+    plt.yscale("log")
     if ytick_flag:
-        plt.yticks([1,10, 100,1000,10000])
+        plt.yticks([1, 10, 100, 1000, 10000])
     else:
-        plt.yticks([1,10,100,1000,10000])
+        plt.yticks([1, 10, 100, 1000, 10000])
         plt.yticks([])
-    plt.title(subplt_title,size=5)
+    plt.title(subplt_title, size=5)
     if del_flag:
         plt.gca().invert_xaxis()
-    plt.savefig(output_name,dpi=1000)
+    plt.savefig(output_name, dpi=1000)
+
+
 def vcf_number_variants(input_vcf_file):
-    with open(input_vcf_file,"r") as f:
-        lines=f.readlines()
-        vcf_variables=VcfVariables.new()
+    with open(input_vcf_file, "r") as f:
+        lines = f.readlines()
+        vcf_variables = VcfVariables.new()
         for line in lines:
             if line[0] != "#":
-                obj=VCFLineSV(line)
+                obj = VCFLineSV(line)
                 if obj.ERROR:
                     continue
-                if obj.SVTYPE=="DEL":
+                if obj.SVTYPE == "DEL":
                     vcf_variables.DEL.append(abs(obj.SVLEN))
                     vcf_variables.DEL_GENOTYPE.append(obj.GENOTYPE)
                     vcf_variables.DEL_AF.append(obj.AF)
-                elif obj.SVTYPE=="INS":
+                elif obj.SVTYPE == "INS":
                     vcf_variables.INS.append(obj.SVLEN)
                     vcf_variables.INS_GENOTYPE.append(obj.GENOTYPE)
                     vcf_variables.INS_AF.append(obj.AF)
-                elif obj.SVTYPE=="INV":
+                elif obj.SVTYPE == "INV":
                     vcf_variables.INV.append(obj.SVLEN)
                     vcf_variables.INV_GENOTYPE.append(obj.GENOTYPE)
                     vcf_variables.INV_AF.append(obj.AF)
-                elif obj.SVTYPE=="DUP":
+                elif obj.SVTYPE == "DUP":
                     vcf_variables.DUP.append(obj.SVLEN)
                     vcf_variables.DUP_GENOTYPE.append(obj.GENOTYPE)
                     vcf_variables.DEL_AF.append(obj.AF)
-                elif obj.SVTYPE=="BND":
+                elif obj.SVTYPE == "BND":
                     vcf_variables.BND.append(obj.SVLEN)
                     vcf_variables.BND_GENOTYPE.append(obj.GENOTYPE)
                     vcf_variables.BND_AF.append(obj.AF)
     return vcf_variables
-
-
-
--- sniffles2_plots.py
+++ sniffles2_plots.py
@@ -9,27 +9,36 @@ import sys
 import os
 import argparse
 
-
 def main():
     parser = argparse.ArgumentParser(description="sample1111")
-    parser.add_argument("-i", "--input", help="Specify the path to the VCF files directory (accepts both single and multi samples VCF files)",required=True)
+    parser.add_argument(
+        "-i",
+        "--input",
+        help="Specify the path to the VCF files directory (accepts both single and multi samples VCF files)",
+        required=True,
+    )
     parser.add_argument("-o", "--output", help="Specify the path to the VCF outp file")
     args = parser.parse_args()
-    input_file_path,output_file_path=args.input,args.output
+    input_file_path, output_file_path = args.input, args.output
     if os.path.isfile(input_file_path):
         if os.path.isdir(output_file_path):
             file_name = os.path.splitext(input_file_path)[0]
             print(file_name)
-            os.system(f"python3 vcf_visulaizer.py -i {input_file_path} -o {output_file_path}")
+            os.system(
+                f"python3 vcf_visulaizer.py -i {input_file_path} -o {output_file_path}"
+            )
     else:
         for file_entry in os.scandir(input_file_path):
-            file_name=file_entry.name
+            file_name = file_entry.name
             if file_name.lower().endswith(".vcf"):
                 file_path = file_entry.path
                 file_name = os.path.splitext(file_name)[0]
                 directory_path = os.path.join(input_file_path, file_name)
                 os.makedirs(directory_path, exist_ok=True)
                 print("Created directory:", directory_path)
-                os.system(f"python3 vcf_visulaizer.py -i {file_path} -o {directory_path}")
+                os.system(
+                    f"python3 vcf_visulaizer.py -i {file_path} -o {directory_path}"
+                )
+
 if __name__ == "__main__":
     main()
